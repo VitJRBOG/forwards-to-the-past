d8d5203dad81e8c46ea2f01bb160713224cf251d
Vitaly Bogryakov <vitjrbog@icloud.com>
2021-12-13 11:29:28 +0500

    Upgrade: described creator of logger

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print('hello world', flush=False)
+# coding: utf-8
+
+import os
+import logging
+
+
+def create_logger(logger_name):
+    logger = logging.getLogger(logger_name)
+
+    msg_format = ''
+
+    file_handler = logging.FileHandler('log.log')
+    if logger_name == 'critical':
+        logger.setLevel(logging.CRITICAL)
+        msg_format = '%(asctime)s - [%(levelname)s] - ' + \
+            '(%(filename)s).%(funcName)s(%(lineno)d) - PID: ' + \
+            '{} - %(message)s'.format(os.getpid())
+    if logger_name == 'warning':
+        logger.setLevel(logging.WARNING)
+        msg_format = '%(asctime)s - [%(levelname)s] - PID: ' + \
+            '{} - %(message)s'.format(os.getpid())
+    else:
+        logger.setLevel(logging.INFO)
+        msg_format = '%(asctime)s - [%(levelname)s] - PID: ' + \
+            '{} - %(message)s'.format(os.getpid())
+    file_handler.setFormatter(logging.Formatter(msg_format))
+
+    logger.addHandler(file_handler)
+
+    return logger
