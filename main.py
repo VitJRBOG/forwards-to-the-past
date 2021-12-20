7069af1066ae387d5d5cf44ad4dba806215240d4
Vitaly Bogryakov <vitjrbog@icloud.com>
2021-12-20 04:34:18 +0000

    Upgrade: described the run functions

--- main.py
+++ main.py
@@ -7,6 +7,39 @@ import configparser
 import hashlib
 import datetime
 import shutil
+import time
+
+import db
+
+
+def main():
+    logging.basicConfig()
+    loggers = {
+        'info': __create_logger('info'),
+        'warning': __create_logger('warning'),
+        'critical': __create_logger('critical')
+    }
+    loggers['info'].info('Program was started')
+
+    __run(loggers)
+
+
+def __run(loggers):
+    while True:
+        config = get_config(loggers)
+        filepaths = __get_list_filepaths(
+            loggers, config['General']['path_to_files'], [])
+        for path in filepaths:
+            hashsum = __get_file_hashsum(loggers, path)
+            con = db.connect(loggers, config)
+            noted_files = db.select_file_by_hashsum(loggers, con, hashsum)
+            if len(noted_files) == 0:
+                modification_date = __get_file_date_modification(loggers, path)
+                backup_path = __copy_file(loggers, path, hashsum, config)
+                file = db.File(hashsum, path, backup_path, modification_date)
+                db.insert_into_file(loggers, con, file)
+
+        time.sleep(float(config['General']['checking_interval']))
 
 
 def __copy_file(loggers, path, hashsum, config):
@@ -25,14 +58,14 @@ def __copy_file(loggers, path, hashsum, config):
 def __get_list_filepaths(loggers, path, filepaths):
 
     try:
-    if os.path.isfile(path):
-        filepaths.append(path)
-    else:
-        for name in os.listdir(path):
-            filepath = os.path.join(path, name)
-            if os.path.isfile(filepath):
-                filepaths.append(filepath)
-            else:
+        if os.path.isfile(path):
+            filepaths.append(path)
+        else:
+            for name in os.listdir(path):
+                filepath = os.path.join(path, name)
+                if os.path.isfile(filepath):
+                    filepaths.append(filepath)
+                else:
                     filepaths = __get_list_filepaths(
                         loggers, filepath, filepaths)
     except Exception:
@@ -45,9 +78,9 @@ def __get_list_filepaths(loggers, path, filepaths):
 def __get_file_hashsum(loggers, path):
     hash = hashlib.md5()
     try:
-    with open(path, 'rb') as f:
-        for chunk in iter(lambda: f.read(128 * hash.block_size), b''):
-            hash.update(chunk)
+        with open(path, 'rb') as f:
+            for chunk in iter(lambda: f.read(128 * hash.block_size), b''):
+                hash.update(chunk)
     except Exception:
         loggers['critical'].exception('Program is terminated')
         sys.exit()
@@ -71,10 +104,10 @@ def get_config(loggers):
     config = configparser.ConfigParser()
 
     try:
-    if os.path.isfile('config.ini'):
-        config.read('config.ini')
-    else:
-        config = __make_default_config(loggers)
+        if os.path.isfile('config.ini'):
+            config.read('config.ini')
+        else:
+            config = __make_default_config(loggers)
     except Exception:
         loggers['critical'].exception('Program is terminated')
         sys.exit()
@@ -131,3 +164,7 @@ def __create_logger(logger_name):
     logger.addHandler(file_handler)
 
     return logger
+
+
+if __name__ == '__main__':
+    main()
