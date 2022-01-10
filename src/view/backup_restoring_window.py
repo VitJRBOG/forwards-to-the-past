51b0391dfa2b484544fa4767c8ebc3ff98065840
Vitaly Bogryakov <vitjrbog@icloud.com>
2022-01-10 06:53:01 +0000

    Update: improved the placing widgets of backup restoring window

--- src/view/backup_restoring_window.py
+++ src/view/backup_restoring_window.py
@@ -7,35 +7,35 @@ import threading
 class Window(tk.Toplevel):
     def __init__(self, master, backups_dates, params_for_btn):
         super().__init__(master)
-        window_width = 300
+        self.title('Восстановление резервной копии')
+
+        window_width = 400
         window_height = 150
 
-        # screen_width = self.winfo_screenwidth()
-        # screen_height = self.winfo_screenheight()
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
 
-        # pos_x = (screen_width // 2) - (window_width // 2)
-        # pos_y = (screen_height // 2) - (window_height // 2)
+        pos_x = (screen_width // 2) - (window_width // 2)
+        pos_y = (screen_height // 2) - (window_height // 2)
 
-        # self.geometry('{}x{}+{}+{}'.format(window_width,
-        #                                    window_height, pos_x, pos_y))
+        self.geometry('{}x{}+{}+{}'.format(window_width,
+                                           window_height, pos_x, pos_y))
         self.minsize(window_width, window_height)
         self.maxsize(window_width, window_height)
-
-        self.title('Восстановление резервной копии')
 
         Frame(self, backups_dates, params_for_btn)
 
 
-class Frame(tk.Frame):
+class Frame(tk.Canvas):
     def __init__(self, master, backups_dates, params_for_btn):
         super().__init__(master)
 
         selected_backup = make_dropdown_list(
-            self, backups_dates, coordinates=(0, 0))
+            self, backups_dates, coordinates=(130, 10))
 
         params_for_btn['args'][1] = selected_backup
         make_button(self, params_for_btn['func'],
-                    params_for_btn['args'], coordinates=(0, 1))
+                    params_for_btn['args'], coordinates=(157, 45))
 
         self.pack()
 
@@ -45,7 +45,7 @@ def make_dropdown_list(master, options, coordinates):
     option.set(options[len(options)-1])
 
     dropdown_list = tk.OptionMenu(master, option, *options)
-    dropdown_list.grid(column=coordinates[0], row=coordinates[1])
+    dropdown_list.place(x=coordinates[0], y=coordinates[1])
 
     return option
 
@@ -55,4 +55,4 @@ def make_button(master, func, func_args, coordinates):
                        command=lambda: threading.Thread(target=func,
                                                         args=(func_args),
                                                         daemon=True).start())
-    button.grid(column=coordinates[0], row=coordinates[1])
+    button.place(x=coordinates[0], y=coordinates[1])
